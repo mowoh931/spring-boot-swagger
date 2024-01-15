0449aad7f7692243fc8a7741d4493712d0120bef
JayMgitM <mowohbaar@yahoo.com>
2024-01-15 12:50:26 -0600

    Api updated

--- src/main/java/com/baar/springbootswagger/api/PersonApi.java
+++ src/main/java/com/baar/springbootswagger/api/PersonApi.java
@@ -1,5 +1,6 @@
 package com.baar.springbootswagger.api;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class PersonApi {
 
-
+@ApiResponse(description = "Returns a message string", responseCode = "200")
     @GetMapping("/")
     public String getMessage() {
         return "Hello there!";
