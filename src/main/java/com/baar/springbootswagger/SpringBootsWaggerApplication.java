7d85c71b22eaf3afe9abf9a8c1cbfaca1cd2f421
JayMgitM <mowohbaar@yahoo.com>
2024-01-14 18:14:56 -0600

    Initial commit

---
+++ src/main/java/com/baar/springbootswagger/SpringBootsWaggerApplication.java
@@ -0,0 +1,13 @@
+package com.baar.springbootswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootsWaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootsWaggerApplication.class, args);
+	}
+
+}
